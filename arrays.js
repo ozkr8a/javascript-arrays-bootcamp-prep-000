33783c103d5cfaaa86f957947335676523d1da7d
Oscar <oscar_ochoac@yahoo.com>
2019-04-09 21:13:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -13,8 +13,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
-  var newArray = array
-  return newArray.unshift(element)
+  return array.unshift(element)
 }
 
 function addElementToEndOfArray(array, element){
